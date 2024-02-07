7d15d04b9c34b7a76e39fd4eb18d6a24ee9123d5
Miguel Augusto Caligares <mcaligares@gmail.com>
2024-02-07 18:19:54 -0300

    add clien-side only rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   css: [
     '@fortawesome/fontawesome-svg-core/styles.css'
   ]
-})
+});
